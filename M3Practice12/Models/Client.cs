5b8e53d3476fdfb6371e10948c7a5852b3c23dba
Anton Lugom <mogul086@mail.ru>
2023-01-13 21:17:44 +0300

    Клиент банка

---
+++ M3Practice12/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Practice12.Models
+{
+
+    /// <summary>
+    /// Клиент банка
+    /// </summary>
+    internal class Client
+    {
+        public static int ClientDBID { get; set; } = 0;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
+        public Client() { }
+
+        public static int GetNextID() => ++ClientDBID;
+
+    }
+}
