d2f48e64a2052c019dc3a75aa91a342efe60c5b9
Anton Lugom <mogul086@mail.ru>
2023-01-14 18:03:02 +0300

    Наполнение БД

--- M3Practice12/Services/DataService.cs
+++ M3Practice12/Services/DataService.cs
@@ -1,8 +1,10 @@
 ﻿using M3Practice12.Models;
+using M3Practice12.Models.Accounts;
 using Newtonsoft.Json;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
-
+using System.Linq;
 
 namespace M3Practice12.Services
 {
@@ -12,6 +14,50 @@ namespace M3Practice12.Services
     internal class DataService
     {
         private const string CLIENTINFO_PATH = "ClientInfo.json";
+
+
+        /// <summary>
+        /// Создание БД при ее отсутствии
+        /// </summary>
+        static DataService()
+        {
+            int client_index = 1;
+
+            var test_clientinfo = Enumerable.Range(1, 10)
+                     .Select(c => new ClientInfo
+                     {
+                         Client = new Client
+                         {
+                             Id = client_index,
+                             Name = $"Имя {client_index}",
+                             Surname = $"Фамилия {client_index}",
+                             Patronymic = $"Отчетсво {client_index}"
+                         },
+                         SavingAccount = new SavingAccount
+                         {
+                             ClientID = client_index,
+                             CreationDate = DateTime.Now,
+                             Number = $"Номер счета {client_index}",
+                             Balance = client_index * 10
+                         },
+                         DepositAccount = new DepositAccount
+                         {
+                             ClientID = client_index,
+                             CreationDate = DateTime.Now,
+                             Number = $"Номер счета {client_index + 10}",
+                             Balance = (client_index++) * 10
+                         }
+
+                     });
+            ObservableCollection<ClientInfo> clients = new ObservableCollection<ClientInfo>();
+
+            foreach (ClientInfo item in test_clientinfo)
+            {
+                clients.Add(item);
+            }
+
+            WriteData(clients);
+        }
 
         /// <summary>
         /// Получение данных с диска
@@ -30,8 +76,7 @@ namespace M3Practice12.Services
         /// <param name="clientDB">Информация для записи</param>
         public static void WriteData(ObservableCollection<ClientInfo> clientDB)
         {
-            File.WriteAllText(CLIENTINFO_PATH,
-                              JsonConvert.SerializeObject(clientDB));
+            File.WriteAllText(CLIENTINFO_PATH, JsonConvert.SerializeObject(clientDB));
         }
     }
 }
