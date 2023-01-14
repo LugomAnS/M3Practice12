1646893124473c241ca136b49c68d0685dc06e33
Anton Lugom <mogul086@mail.ru>
2023-01-14 14:42:21 +0300

    Начало MainWindowViewModel

--- M3Practice12/Services/DataService.cs
+++ M3Practice12/Services/DataService.cs
@@ -1,12 +1,8 @@
 ﻿using M3Practice12.Models;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace M3Practice12.Services
 {
---
+++ M3Practice12/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using M3Practice12.Models;
+using M3Practice12.Services;
+using M3Practice12.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Practice12.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Поля и свойства
+
+        #region Информация о всех клиентах
+
+        private ObservableCollection<ClientInfo> _clients = new ObservableCollection<ClientInfo>();
+        public ObservableCollection<ClientInfo> Clients 
+        {
+            get => _clients;
+            set => Set(ref _clients, value);
+        }
+
+        #endregion
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            Clients = DataService.GetData();
+        }
+
+        #region Команды
+
+        #endregion V
+    }
+}
