413ca5f3bee461f725ac1a6906b58d569921e104
Anton Lugom <mogul086@mail.ru>
2023-01-14 14:20:26 +0300

    Запись и считывания данных с диска (JSON)

---
+++ M3Practice12/Services/DataService.cs
@@ -0,0 +1,41 @@
+﻿using M3Practice12.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Practice12.Services
+{
+    /// <summary>
+    /// Сервис работы с данными 
+    /// </summary>
+    internal class DataService
+    {
+        private const string CLIENTINFO_PATH = "ClientInfo.json";
+
+        /// <summary>
+        /// Получение данных с диска
+        /// </summary>
+        /// <returns>ObservableCollection<ClientInfo></returns>
+        public static ObservableCollection<ClientInfo> GetData()
+        {
+            string json = File.ReadAllText(CLIENTINFO_PATH);
+
+            return JsonConvert.DeserializeObject<ObservableCollection<ClientInfo>>(json);
+        }j
+
+        /// <summary>
+        /// Запись информации на диск
+        /// </summary>
+        /// <param name="clientDB">Информация для записи</param>
+        public static void WriteData(ObservableCollection<ClientInfo> clientDB)
+        {
+            File.WriteAllText(CLIENTINFO_PATH,
+                              JsonConvert.SerializeObject(clientDB));
+        }
+    }
+}
